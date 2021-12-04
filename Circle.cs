69f4b848954a23769914cb79cbb36b586c2950c6
ImAClownAndDontComplain <94308517+ImAClownAndDontComplain@users.noreply.github.com>
2021-12-04 19:55:17 +0500

    circle class
    Signed-off-by: ImAClownAndDontComplain <94308517+ImAClownAndDontComplain@users.noreply.github.com>

---
+++ Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bless_n_keep4._1
+{
+    class Circle
+    {
+        private int x;
+        private int y;
+        private int R=10;
+
+        public Circle(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int getx() { return x; }
+        public int gety() { return y; }
+        public int getR() { return R; }
+        public void setC(int _x,int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        public void setR(int _R)
+        {
+            R = _R;
+        }
+        public void draw(MouseEventArgs e)
+        {
+            Pen blackPen = new Pen(Color.Black, 3);
+            e.Graphics.DrawEllipse(blackPen, x, y, 2*R, 2*R);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bless_n_keep4._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Circle C = new Circle(e.X, e.Y);
+
+        }
+    }
+}
